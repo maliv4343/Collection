543ea59fd3e0122c43cc47b4879bdcebf6bfbbf8
vijaymali <maliv4343@gmail.com>
2023-01-10 11:57:50 +0530

    upload second project collection

---
+++ src/com/example/CollectionDemo.java
@@ -0,0 +1,19 @@
+package com.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CollectionDemo {
+
+	public static void main(String[] args) {
+		
+		List<String> list=new ArrayList<String>();
+
+		list.add("acer");
+		list.add("lenovo");
+		list.add("mac");
+		
+		System.out.println(list);
+	}
+
+}
